fc94158b5b99b9e6af75f689dc53747311345e2b
Marco Aurélio Lima Fernandes <malf88@gmail.com>
2023-03-07 23:07:38 -0300

    Primeiro commit

---
+++ src/Provider/ModuleServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Provider;
+
+use App\Application\Abstracts\ServiceProviderAbstract;
+use App\Domain\Projects\Business\ProjectBusiness;
+use App\Domain\Projects\Contracts\ProjectBusinessInterface;
+use App\Domain\Projects\Contracts\ProjectRepositoryInterface;
+use App\Domain\Projects\Repository\ProjectRepository;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider
+    extends ServiceProviderAbstract
+    implements DeferrableProvider
+{
+    public array $bindings = [
+
+    ];
+
+
+
+
+}
